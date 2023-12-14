31bade2114b5df8cd40e03991ff4c3bc69599cc9
Eric <perivaldoeric@gmail.com>
2023-12-14 09:19:48 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+let NomeHeroi = "Sr dos Bares"
 let XP  = 9999
-let NomeHeroi = "Ericdx"
 
-if(Nivel){
-    XP <= 1000 === "Ferro"
+let Nivel
+
+switch (true) {
+    case XP <= 1000:
+        Nivel = "Ferro";
+        break;
+
+        case XP >= 1001 && XP <= 2000:
+        Nivel = "Bronze";
+        break;
+
+        case XP >= 2001 && XP <= 5000:
+        Nivel = "Prata";
+        break;
+
+        case XP >= 5001 && XP <= 7000:
+        Nivel = "Ouro";
+        break;
+
+        case XP >= 7001 && XP <= 8000:
+        Nivel = "Platina";
+        break;
+
+        case XP >= 8001 && XP <= 9000:
+        Nivel = "Ascendente";
+        break;
+
+        case XP >= 9001 && XP <= 10000:
+        Nivel = "Imortal";
+        break;
+
+        case XP > 10001:
+        Nivel = "Radiante";
+        break;
+
+        default:
+            Nivel = "Desconhecido";
+
 }
-console.log("O Herói de nome" + NomeHeroi + "esta no nivel de" + Nivel )
+
+
+console.log("O Herói de nome " + NomeHeroi + " esta no nivel de " + Nivel)
